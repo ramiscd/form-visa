6453f6c4463888b2b77f246295a12f4d927afa52
Ramis Damasceno <ramis1998d@gmail.com>
2026-03-27 01:59:30 -0300

    update url

--- front/services/api.ts
+++ front/services/api.ts
@@ -1,4 +1,5 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api'
+//const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api'
+const API_BASE_URL = 'https://form-visa-back.onrender.com'
 
 
 class ApiClient {
